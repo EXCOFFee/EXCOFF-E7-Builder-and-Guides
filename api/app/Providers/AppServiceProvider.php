8d74c091bd59a285227cb6b0bb95c5f9ee351b3e
EXCOFFee <santiagoexcofier@gmail.com>
2025-12-17 23:53:23 -0300

    fix: Add Discord OAuth driver (socialiteproviders/discord)
    - Installed socialiteproviders/discord v4.2
    - Registered Discord event listener in AppServiceProvider
    - Fixes: 'Driver [discord] not supported' error

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\Discord\DiscordExtendSocialite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register Discord OAuth driver
+        Event::listen(function (SocialiteWasCalled $event) {
+            $event->extendSocialite('discord', \SocialiteProviders\Discord\Provider::class);
+        });
     }
 }
